dc45b09524062c34af9391e152fc99e8e5477253
WojtK <48882476+WojtK@users.noreply.github.com>
2019-07-24 12:12:06 +0200

    Update Task_6(Permutation).cs

--- Task_6(Permutation).cs
+++ Task_6(Permutation).cs
@@ -1,7 +1,7 @@
 public static void Permutation()
         {
             Console.WriteLine("Array1: ");
-            int[] arr1 = new int[11];
+            int[] arr1 = new int[11]; //kazdy ciag liczb ma 11 elementow, stad tworzymy dwie tablice, ktore wypelniamy
             for (int j = 0; j < arr1.Length; j++)
                 arr1[j] = int.Parse(Console.ReadLine());
             Console.WriteLine("Array2: ");
@@ -9,13 +9,13 @@ public static void Permutation()
             for (int j = 0; j < arr2.Length; j++)
                 arr2[j] = int.Parse(Console.ReadLine());
 
-            Array.Sort(arr1);
-            Array.Sort(arr2);
+            Array.Sort(arr1);  
+            Array.Sort(arr2); // sortujemy obie
             for (int i = 0; i < 11; i++)
-                if (arr1[i] != arr2[i])
+                if (arr1[i] != arr2[i]) // porownujemy, jesli gdzies sie roznia to nie jest mozliwa permutacja
                 {
                     Console.WriteLine("NO");
-                    goto finish;
+                    goto finish; //w przypadku roznicy miedzy tablicami, przechodzimy do finish:
                 }
             Console.WriteLine("YES");
             finish:;

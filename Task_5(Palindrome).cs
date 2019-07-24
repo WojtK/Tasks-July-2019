0847f4f3f59f3fdf5b6e3614e1705438ecdb24de
WojtK <48882476+WojtK@users.noreply.github.com>
2019-07-24 12:10:06 +0200

    Update Task_5(Palindrome).cs

--- Task_5(Palindrome).cs
+++ Task_5(Palindrome).cs
@@ -1,12 +1,10 @@
 public static void Palindrom(string word)
         {
-            Console.WriteLine(word);
             Regex rgx = new Regex("[^a-zA-Z]");
-            word = rgx.Replace(word, "").ToLower();
-            Console.WriteLine(word);
-            string reversed = new string(word.Reverse().ToArray());
+            word = rgx.Replace(word, "").ToLower(); //usuwamy wszelkie "zle znaki"
+            string reversed = new string(word.Reverse().ToArray()); // tworzymy nowy string, ktory jest odwrocona wersja word
             Console.WriteLine(reversed);
-            if (word == reversed)
+            if (word == reversed)  //porownujemy
                 Console.WriteLine("YES");
             else
                 Console.WriteLine("NO");

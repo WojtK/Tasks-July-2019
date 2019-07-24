0d69df4a99f8bddd0e2817f24af8dcd8e4d69319
WojtK <48882476+WojtK@users.noreply.github.com>
2019-07-24 12:08:37 +0200

    Update Task_4(ReverseArray).cs

--- Task_4(ReverseArray).cs
+++ Task_4(ReverseArray).cs
@@ -1,12 +1,13 @@
 public static void ReverseArray()
         {
+            //Wprowadzamy rozmiar, deklarujemy tablice i wypelniamy wartosciami
             Console.WriteLine("enter the size: ");
             int n = int.Parse(Console.ReadLine());
             int[] arr = new int[n];
 
             for (int j = 0; j < arr.Length; j++)
                 arr[j] = int.Parse(Console.ReadLine());
-
-            for (int i = arr.Length; i > 0; i--)
-                Console.Write(arr[i - 1] + " ");
+            
+            for (int i = arr.Length; i > 0; i--) //Odwraca petle wypisujac ja od tylu
+                Console.Write(arr[i - 1] + " "); //Zamiast petli mozna bylo tez uzyc Array.Reverse(arr) i wypisac tablice arr
         }

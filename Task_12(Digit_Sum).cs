33aad6ff3ac7084016a643a803148aac8f14e3fc
WojtK <48882476+WojtK@users.noreply.github.com>
2019-07-24 12:51:36 +0200

    Update Task_12(Digit_Sum).cs

--- Task_12(Digit_Sum).cs
+++ Task_12(Digit_Sum).cs
@@ -1,25 +1,26 @@
  public static void Digit_Sum()
         {
-            int n = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine()); //wczytujemy rozmiar
             string s;          
             int[] tab = new int[256];
             string[] tab1 = new string[256];
-            s = Console.ReadLine();
-            tab1 = s.Split(' ');
+            s = Console.ReadLine(); // wczytujemy dane w jednej linii
+            tab1 = s.Split(' '); //dzielimy string 's' na czesci, ktore przypisujemy tab1
                 for (int i = 0; i < n; i++)
                 {
                     int sum = 0;
-                    int g = int.Parse(tab1[i]);
+                    int g = int.Parse(tab1[i]); //pobieramy wartosc z tab1 i sumujemy jej cyfry
                     while (g != 0)
                     {
                         sum += g % 10;
                         g /= 10;
                     }
-                    tab[i] = sum;
+                    tab[i] = sum; //zwracamy otrzymany wynik do tab 
                     
-                    Console.WriteLine("Sum of digit index number: "+i+" = "+sum);
+                    Console.WriteLine("Sum of digit index number: "+i+" = "+sum); //wypisujemy sume liczb dla poszczegolnego indexu
                     Console.WriteLine("---------");
                 }               
 
             Console.WriteLine("The maximum digit sum -> index number: "+Array.IndexOf(tab, tab.Max()));
+                //wypisujemy index, pod ktorym wystepuje najwieksza wartosc z zsumowanych liczb
             }

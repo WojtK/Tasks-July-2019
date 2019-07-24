bc2d215fab43b4fdf97c012f09e18f48e1f6849e
WojtK <48882476+WojtK@users.noreply.github.com>
2019-07-24 12:37:34 +0200

    Update Task_8(Finding_Primes).cs

--- Task_8(Finding_Primes).cs
+++ Task_8(Finding_Primes).cs
@@ -1,7 +1,7 @@
  public static void Finding_Primes()
         {
             int m = int.Parse(Console.ReadLine());
-            int n = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine()); //wczytujemy zakresy
 
             bool prime_number = true;
             int score = 0;
@@ -9,20 +9,21 @@
 
             for (int i = m; i <= n; i++)
             {
+                // sprawdzamy czy liczba dzieli sie nie tylko przez 1 i sama siebie
                 for (j = 2; j < i; j++)
                 {
-                    if (i % j == 0)
+                    if (i % j == 0) //jezeli nie dzieli sie tylko przez siebie, to nie jest to liczba pierwsza
                     {
                         prime_number = false;
                     }
                 }
                 if ((prime_number == true) && (i > 1))
                 {
-                    score++; 
+                    Console.WriteLine("Prime number is: " + i); //wypisujemy liczby pierwsze
+                    score++; //wypisujac dodajemy kolejne wystapienie liczby pierwszej
                 }
 
                 else prime_number = true;
-
             }
-            Console.WriteLine(score);
+            Console.WriteLine(score); //wynik
         }

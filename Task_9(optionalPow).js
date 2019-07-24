26d02de9a003502a5faf6b8e1aa5eee17d55dcb9
WojtK <48882476+WojtK@users.noreply.github.com>
2019-07-24 12:39:57 +0200

    Update Task_9(optionalPow).js

--- Task_9(optionalPow).js
+++ Task_9(optionalPow).js
@@ -1,12 +1,12 @@
  function optionalPow(a,b){
-        var score = 1;
-      if(document.getElementById('OK_button').onclick)
+        var score = 1; // wartosc domyslna
+      if(document.getElementById('OK_button').onclick) //sprawdza czy nie zostal klikniety przycisk OK (domyslnie 'OK_button')
           {
               score = Math.pow(a,b);
           }
-      if(document.getElementById('Cancel_button').onclick)
+      if(document.getElementById('Cancel_button').onclick) //sprawdza czy nie zostal klikniety przycisk CANCEL (domyslnie 'Cancel_button')
           {
-              score = Math.pow(b,a);
+              score = Math.pow(b,a); 
           }
-        return score;
+        return score; // zwraca wynik potegowania
   }

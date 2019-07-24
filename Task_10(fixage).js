247eddca7b0c20cadf52ae12d4b38503374a0062
WojtK <48882476+WojtK@users.noreply.github.com>
2019-07-24 12:42:13 +0200

    Update Task_10(fixage).js

--- Task_10(fixage).js
+++ Task_10(fixage).js
@@ -1,21 +1,21 @@
-fixage(tab)
+fixage(tab) //pobiera podana tablice
     {
-        var newtab = [];
+        var newtab = []; //deklarujemy pusta tablice
 		var s = "";
         for(var i =0; i < tab.length; i++)
             {
-                if(tab[i] >= 18 && tab[i] <= 60)
+                if(tab[i] >= 18 && tab[i] <= 60) //sprawdzamy czy dany indeks tablicy nie ma przypadkiem poszukiwanej przez nas wartosci
                     {
-                       newtab.push(tab[i]);
+                       newtab.push(tab[i]); // jestli tak to dodajemy go do tablicy newtab
                     }
             }
         for(var i=0; i < newtab.length; i++) 
         {
-          s+=newtab[i]+","; 
+          s+=newtab[i]+","; //wypisujemy newtab
         }
         if (newtab[0] == null)
 		{
-			console.log("NA");
+			console.log("NA"); //jesli newtab jest puste, wypisujemy "NA"
 		}else
           console.log(s); 
     }

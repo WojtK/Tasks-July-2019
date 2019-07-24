7a1c2b72fc7be9ab05e330c55d5b54d679e1c5ce
WojtK <48882476+WojtK@users.noreply.github.com>
2019-07-24 12:48:50 +0200

    Update Task_11(Common_Digit).cs

--- Task_11(Common_Digit).cs
+++ Task_11(Common_Digit).cs
@@ -1,19 +1,19 @@
 public static void Common_Digit()
         {
-            int n = int.Parse(Console.ReadLine());
             string s = "";
             int max = 0;
+            int n = int.Parse(Console.ReadLine()); //wczytujemy rozmiar
             int[] tab = new int[256];
-            s = Console.ReadLine();
-            Char score = Char.MaxValue;
+            s = Console.ReadLine(); //wczytujemy dane z jednej linii
+            Char score = Char.MaxValue; 
             Array.Clear(tab, 0, tab.Length - 1);
 
             foreach (Char c in s)
-                if(c!=' ' && ++tab[c] > max)
+                if(c!=' ' && ++tab[c] > max) // szukamy najczesciej wystepujacej liczby
             {                              
-                    Console.Write(c + " ");
-                    max = tab[c];
+                    Console.Write(c + " "); //wypisuje aktualnie najczesciej spotkana liczbe
+                    max = tab[c]; 
                     score = c;               
             }            
-            Console.Write("\n" + score);
+            Console.Write("\n" + score); //wypisuje liczbe o najwiekszej ilosci wystapien
         }
